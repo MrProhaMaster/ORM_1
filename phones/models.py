5ca6fbea98d1570e5d60d17b495fe3e0f89f8b6c
MrProha <mrprohagames@gmail.com>
2023-01-05 15:38:39 +0300

    finally done

---
+++ phones/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Phone(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    image = models.URLField(max_length=200)
+    release_date = models.DateField(auto_now=False, auto_now_add=False)
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=150, unique=True)
